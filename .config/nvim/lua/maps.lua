15ed800f45d598087e40a41a6d4dc610425c5f2b
lamhungypl <lamhungypl@gmail.com>
2023-01-27 08:29:51 +0700

    stuff

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,6 +1,13 @@
 local keymap = vim.keymap
 
--- insert mode keys bindings
+-- insert mode keys binding
 keymap.set('i', 'jk' , '<Esc>')
 keymap.set('i', 'ww', '<Esc>:w<CR>')
 keymap.set('i', 'qq', '<Esc>:q!<CR>')
+
+-- normal mode keys bindings
+keymap.set('n', '<C-a>', 'ggVG')
+
+
+-- visualize mode keys bindings
+keymap.set('v', 'L', '$')
